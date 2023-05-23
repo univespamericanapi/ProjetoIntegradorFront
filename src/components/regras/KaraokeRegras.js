bbdd3bf07f6574e7dab6f0ab3f511d85ce81739a
Guilherme C de Lima <73154617+gcelestinodelima@users.noreply.github.com>
2023-05-23 01:23:26 -0300

    formatação de html
    formatei de acordo com o modelo enviado na entrevista

--- src/components/regras/KaraokeRegras.js
+++ src/components/regras/KaraokeRegras.js
@@ -13,94 +13,148 @@ export class KaraokeRegras extends Component {
 					borderBottom: '1px solid #000',
 				}}
 			>
-				REGULAMENTO DO CONCURSO ANIME FEST DE KARAOKÊ 2023 INTRODUÇÃO O Concurso
-				Anime Fest de Karaokê é um concurso de canto onde são apresentadas
-				canções ou músicas de qualquer origem ou língua, ficando proibido apenas
-				a apresentação de músicas ou canções com letras improprias a menores de
-				18 anos; A música deve ser de um idioma real, podendo ser cantada no
-				idioma original ou na sua versão adaptada para diferentes idiomas; O
-				concurso é aberto para pessoas com idade superior a 03 anos, e as
-				apresentações podem ser feitas tanto individual quanto em duplas e
-				grupos; INSCRIÇÃO As inscrições podem ser realizadas em dois momento: -
-				Inscrição online: Através de formulário pela internet. O início das
-				inscrições e liberação do link do formulário de inscrição serão
-				anunciados com antecedência em nossa página do Facebook
-				(www.facebook.com/CircuitoAnimeFest) e Instagram
-				(https://www.instagram.com/animefest/). No cabeçalho do formulário será
-				informado a quantidade máxima de vagas no concurso e data e horário
-				limite das inscrições online. Após a inscrição realizada, o participante
-				será contatado pelo Whatsapp informado na inscrição em até 48 horas. A
-				presença deve ser confirmada, somente por membros do grupo, no dia do
-				evento até 30 (trinta) minutos antes do início do Concurso de Karaokê na
-				fila exclusiva de concursos ou tenda de inscrições, próximo ao palco
-				Anime Fest. A não confirmação no dia, provoca sua desclassificação.
-				Informações requeridas no momento da inscrição: E-mail, Nome, Nome
-				social ou artístico, Documento (RG/CPF/CNH), Data de Nascimento,
-				Telefone (Whatsapp), Cidade e Estado, Nome da Música, Origem da Música,
-				Link para a Música. - Inscrições no dia do evento: Abrem às 11h00 e vão
-				até 30 (trinta) minutos antes do início do Concurso de Karaokê. Só
-				teremos inscrições abertas no dia caso ainda haja vagas remanescentes da
-				inscrição online ou para lista de espera. Não aceitaremos inscrição por
-				procuração ou representação. Caso queira saber se ainda temos vagas
-				(para apresentação ou lista de espera), entre em contato pelo Whatsapp
-				(19) 98933-4566. Não esqueça de levar no dia do evento: - Documento com
-				foto (mesmo que utilizado no momento da inscrição). - Pendrive ou
-				celular contendo seu áudio (somente em MP3) a ser utilizado na
-				apresentação. O concurso só ocorrerá se 03 (três) participantes ou mais
-				tiverem sido inscritos. No caso de apenas 02 (dois) ou menos, os
-				participantes poderão se apresentar mas ganharão apenas prêmios por
-				participação. APRESENTAÇÃO A sequência das apresentações no dia do
-				evento será correspondente ao recebimento das inscrições. As músicas
-				apresentadas pelos participantes devem ter no máximo 5 (cinco) minutos
-				de duração e estar no formato MP3; caso o arquivo de áudio exceda o
-				tempo estabelecido, será necessário envio de edição da música dentro do
-				tempo estabelecido. Participante prejudicado por falhas na aparelhagem
-				de som ou queda da rede elétrica, poderá reiniciar sua apresentação ou
-				repeti-la ao final das apresentações. Os grupos devem se apresentar ao
-				lado do Palco Anime Fest 20 minutos antes do horário marcado para início
-				do Concurso de Karaokê. A organização não disponibilizará tomadas ou
-				qualquer instalação elétrica auxiliar no palco e não se responsabiliza
-				por equipamentos ou itens levados pelos grupos participantes. AVALIAÇÃO
-				O corpo de júri será composto de 03 (três) juízes sendo: musicistas,
-				vocalistas com experiência comprovada, intérpretes e entusiastas; cujas
-				notas serão consideradas plenas e também inquestionáveis. Cada membro
-				dará a cada participante 03 (três) notas: por Afinação, Tempo e
-				Interpretação. As notas serão de 50 (cinquenta) a 100 (cem), podendo ser
-				apenas fracionadas em 5 (cinco) pontos. A nota 00 (zero) será dada
-				apenas aos participantes desclassificados; o cálculo da nota final é
-				realizado através de uma média aritmética entre todas as notas de cada
-				um dos membros do júri. Vence o participante com maior nota final em
-				cada categoria e os demais são classificados de forma decrescente. Os
-				desempates serão feitos pelos seguintes critérios e ordens: 01 - Média
-				geral (notas dos três júris) no quesito Afinação; 02 - Média geral no
-				quesito Tempo; 03 - Média geral no quesito Interpretação; 04 - Indicação
-				do júri. PREMIAÇÃO Serão premiados com medalha e kit de produtos dos
-				expositores do evento o primeiro, segundo e terceiro lugar Os vencedores
-				serão anunciados no palco e os prêmios serão entregues no ato; caso
-				estejam ausentes, o prêmio pode ser retirado em local a combinar até no
-				máximo 01 (um) mês após o evento em questão. A premiação não será
-				enviada pelo correio. PROIBIÇÕES É PROIBIDO E PROVOCA A DESCLASSIFICAÇÃO
-				DO CONCURSO: O uso de cenários e/ou equipamentos que possam pôr em risco
-				o grupo, o local, o público ou outros participantes; participação de
-				jurados e equipe organizadora do evento; Uso de qualquer tipo de
-				ofensas, constrangimentos ou apelo sexual e que firam as disposições
-				criminais; Atirar qualquer objeto para fora do palco ou pularem do
-				palco. Porte e uso dos seguintes itens: fogo, explosivos, fogos de
-				artifício, arma de fogo (mesmo descarregadas), armas brancas, líquidos
-				ou materiais viscosos, e qualquer objeto que possa representar perigo às
-				pessoas presentes; Uso de qualquer banner ou faixa divulgando algum site
-				ou empresa, sem autorização da organização do evento; Uso de
-				objetos/materiais como fonte sonora (instrumentos musicais, sinos,
-				caixas de som, buzinas, geradores de ruído, pocs, etc.). DISPOSIÇÕES
-				GERAIS Os participantes cedem o direito de vídeo-imagem por prazo
-				indeterminado, para fins promocionais do Concurso de Karaokê, para
-				Avalon Eventos e seus parceiros; Ao participar do concurso os
-				participantes autorizam o envio de e-mails marketing com novidades sobre
-				os eventos; Os inscritos devem pagar normalmente a entrada do evento
-				para participar do concurso; Os casos omissos no regulamento serão
-				analisados pela coordenação e equipe organizadora do Concurso de Karaokê
-				e a sua decisão será inquestionável. As regras podem sofrer alterações
-				sem aviso prévio. Última alteração: 28/03/2023.
+				<h3>REGULAMENTO DO CONCURSO ANIME FEST DE KARAOKÊ 2023</h3> 
+				<br />
+				<h4>1. INTRODUÇÃO</h4> 
+				<p>
+					O Concurso Anime Fest de Karaokê é um concurso de canto onde são apresentadas
+					canções ou músicas de qualquer origem ou língua, ficando proibido apenas
+					a apresentação de músicas ou canções com letras improprias a menores de
+					18 anos; A música deve ser de um idioma real, podendo ser cantada no
+					idioma original ou na sua versão adaptada para diferentes idiomas; O
+					concurso é aberto para pessoas com idade superior a 03 anos, e as
+					apresentações podem ser feitas tanto individual quanto em duplas e
+					grupos;	
+				</p>  
+				<br />
+				<h4>2. INSCRIÇÃO</h4>
+				<p>
+					As inscrições podem ser realizadas em dois momento:
+					<br /> 
+					<span className="underline">- Inscrição online:</span> Através de formulário pela internet. O início das
+					inscrições e liberação do link do formulário de inscrição serão anunciados com antecedência em nossa página do Facebook
+					(<a href="www.facebook.com/CircuitoAnimeFest">
+						www.facebook.com/CircuitoAnimeFest
+					</a>) e Instagram(
+					<a href="https://www.instagram.com/animefest/">
+					https://www.instagram.com/animefest/
+					</a>). No cabeçalho do formulário será
+					informado a quantidade máxima de vagas no concurso e data e horário
+					limite das inscrições online. Após a inscrição realizada, o participante
+					será contatado pelo Whatsapp informado na inscrição em até 48 horas. A
+					presença deve ser confirmada, somente por membros do grupo, no dia do
+					evento até 30 (trinta) minutos antes do início do Concurso de Karaokê na
+					fila exclusiva de concursos ou tenda de inscrições, próximo ao palco
+					Anime Fest. <strong>A não confirmação no dia, provoca sua desclassificação.</strong>
+				</p> 
+					<br />
+				<p>
+					<strong>Informações requeridas no momento da inscrição:</strong> 
+					<br />
+					E-mail, Nome, Nome
+					social ou artístico, Documento (RG/CPF/CNH), Data de Nascimento,
+					Telefone (Whatsapp), Cidade e Estado, Nome da Música, Origem da Música,
+					Link para a Música.
+				</p>
+				<br />
+				<p> 
+					<span className="underline">- Inscrições no dia do evento:</span> Abrem às 11h00 e vão
+					até 30 (trinta) minutos antes do início do Concurso de Karaokê. Só
+					teremos inscrições abertas no dia caso ainda haja vagas remanescentes da
+					inscrição online ou para lista de espera. Não aceitaremos inscrição por
+					procuração ou representação. Caso queira saber se ainda temos vagas
+					(para apresentação ou lista de espera), entre em contato pelo Whatsapp
+					(19) 98933-4566. 
+				</p>
+				<br />
+				<p>
+					<span className="underline">Não esqueça de levar no dia do evento:</span>
+					<br />
+					 - Documento com foto (mesmo que utilizado no momento da inscrição). - Pendrive ou
+					celular contendo seu áudio (somente em MP3) a ser utilizado na
+					apresentação. 
+				</p>
+				<br />
+				<p>
+					O concurso só ocorrerá se 03 (três) participantes ou mais
+					tiverem sido inscritos. No caso de apenas 02 (dois) ou menos, os
+					participantes poderão se apresentar mas ganharão apenas prêmios por
+					participação.
+				</p>
+				<br />
+				<h4>3. APRESENTAÇÃO</h4> 
+				<p>
+					A sequência das apresentações no dia do
+					evento será correspondente ao recebimento das inscrições. <strong>As músicas
+					apresentadas pelos participantes devem ter no máximo 5 (cinco) minutos
+					de duração e estar no formato MP3</strong>; caso o arquivo de áudio exceda o
+					tempo estabelecido, será necessário envio de edição da música dentro do
+					tempo estabelecido. Participante prejudicado por falhas na aparelhagem
+					de som ou queda da rede elétrica, poderá reiniciar sua apresentação ou
+					repeti-la ao final das apresentações. Os grupos devem se apresentar ao
+					lado do Palco Anime Fest 20 minutos antes do horário marcado para início
+					do Concurso de Karaokê. A organização não disponibilizará tomadas ou
+					qualquer instalação elétrica auxiliar no palco e não se responsabiliza
+					por equipamentos ou itens levados pelos grupos participantes. 
+				</p>
+				<br />
+				<h4>4. AVALIAÇÃO</h4>
+				<p>
+					O corpo de júri será composto de 03 (três) juízes sendo: musicistas,
+					vocalistas com experiência comprovada, intérpretes e entusiastas; cujas
+					notas serão consideradas plenas e também inquestionáveis. Cada membro
+					dará a cada participante 03 (três) notas: por <strong>Afinação, Tempo e
+					Interpretação.</strong> As notas serão de 50 (cinquenta) a 100 (cem), podendo ser
+					apenas fracionadas em 5 (cinco) pontos. A nota 00 (zero) será dada
+					apenas aos participantes desclassificados; o cálculo da nota final é
+					realizado através de uma média aritmética entre todas as notas de cada
+					um dos membros do júri. Vence o participante com maior nota final em
+					cada categoria e os demais são classificados de forma decrescente. Os
+					desempates serão feitos pelos seguintes critérios e ordens: 01 - Média
+					geral (notas dos três júris) no quesito Afinação; 02 - Média geral no
+					quesito Tempo; 03 - Média geral no quesito Interpretação; 04 - Indicação
+					do júri. 
+				</p>
+				<br />
+				<h4>5. PREMIAÇÃO</h4>
+				<p>  
+					Serão premiados com medalha e kit de produtos dos
+					expositores do evento o primeiro, segundo e terceiro lugar Os vencedores
+					serão anunciados no palco e os prêmios serão entregues no ato; caso
+					estejam ausentes, o prêmio pode ser retirado em local a combinar até no
+					máximo 01 (um) mês após o evento em questão. A premiação não será
+					enviada pelo correio. 
+				</p>
+				<br />
+				<h4>6. PROIBIÇÕES</h4>
+				<h4>É PROIBIDO E PROVOCA A DESCLASSIFICAÇÃO DO CONCURSO:</h4>
+				<p>
+					O uso de cenários e/ou equipamentos que possam pôr em risco
+					o grupo, o local, o público ou outros participantes; participação de
+					jurados e equipe organizadora do evento; Uso de qualquer tipo de
+					ofensas, constrangimentos ou apelo sexual e que firam as disposições
+					criminais; Atirar qualquer objeto para fora do palco ou pularem do
+					palco. Porte e uso dos seguintes itens: fogo, explosivos, fogos de
+					artifício, arma de fogo (mesmo descarregadas), armas brancas, líquidos
+					ou materiais viscosos, e qualquer objeto que possa representar perigo às
+					pessoas presentes; Uso de qualquer banner ou faixa divulgando algum site
+					ou empresa, sem autorização da organização do evento; Uso de
+					objetos/materiais como fonte sonora (instrumentos musicais, sinos,
+					caixas de som, buzinas, geradores de ruído, pocs, etc.). 
+				</p>
+				<br />
+				<h4>7. DISPOSIÇÕES GERAIS</h4>
+				<p>
+					Os participantes cedem o direito de vídeo-imagem por prazo
+					indeterminado, para fins promocionais do Concurso de Karaokê, para
+					Avalon Eventos e seus parceiros; Ao participar do concurso os
+					participantes autorizam o envio de e-mails marketing com novidades sobre
+					os eventos; Os inscritos devem pagar normalmente a entrada do evento
+					para participar do concurso; Os casos omissos no regulamento serão
+					analisados pela coordenação e equipe organizadora do Concurso de Karaokê
+					e a sua decisão será inquestionável. As regras podem sofrer alterações
+					sem aviso prévio. Última alteração: 28/03/2023.
+				</p>
 			</Box>
 		);
 	}
