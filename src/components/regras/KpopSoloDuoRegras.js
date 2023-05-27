9c01300b7dd16157df9b0513c67f0a16f1dd459e
Guilherme C de Lima <73154617+gcelestinodelima@users.noreply.github.com>
2023-05-26 23:47:49 -0300

    formatação do html
    formatação de acordo com o documento de referencia.

--- src/components/regras/KpopSoloDuoRegras.js
+++ src/components/regras/KpopSoloDuoRegras.js
@@ -13,118 +13,189 @@ export class KpopSoloDuoRegras extends Component {
 					borderBottom: '1px solid #000',
 				}}
 			>
-				REGULAMENTO DO CONCURSO KPOP SOLO E DUO 2023 INTRODUÇÃO O Concurso Kpop
-				Solo e Duos é um concurso de dança pop coreana onde os participantes
-				fazem apresentações individuais ou em dupla. Qualquer pessoa com mais de
-				10 anos pode participar. São bem vindos profissionais,
-				pré-profissionais, amadores e iniciantes. No momento da inscrição os
-				participantes escolhem entre a modalidade Solo e Duo. Cada participante
-				poderá participar de apenas uma apresentação por concurso, não sendo
-				permitido que o mesmo participante faça apresentações como solo e duo.
-				INSCRIÇÃO As inscrições podem ser realizadas em dois momento: -
-				Inscrição online: Através de formulário pela internet. O início das
-				inscrições e liberação do link do formulário de inscrição serão
-				anunciados com antecedência em nossa página do Facebook
-				(www.facebook.com/CircuitoAnimeFest) e Instagram
-				(https://www.instagram.com/animefest/). No cabeçalho do formulário será
-				informado a quantidade máxima de vagas no concurso e data e horário
-				limite das inscrições online. Após a inscrição realizada, o participante
-				será contatado pelo Whatsapp informado na inscrição em até 48 horas. A
-				presença deve ser confirmada, somente pela pessoa que fez a inscrição,
-				no dia do evento até 30 (trinta) minutos antes do início do Concursos
-				Kpop Solo e Duo na fila exclusiva de concursos ou tenda de inscrições,
-				próximo ao palco Anime Fest. A não confirmação no dia, provoca sua
-				desclassificação. Dados necessários para efetuar a inscrição: E-mail,
-				Nome, Nome social ou artístico, Documento (RG/CPF/CNH), Data de
-				Nascimento, Telefone (Whatsapp), Cidade e Estado, Modalidade (Solo ou
-				Duo), Nome do Integrante 02. Nome da Música, Origem da Música, Link para
-				a Música, Link para Referência de Figurino (não obrigatório). Vide anexo
-				01 do regulamento (ao final do documento) para detalhes sobre Referência
-				de Figurino. - Inscrições no dia do evento: Abrem às 11h00 e vão até 30
-				(trinta) minutos antes do início do Concursos Kpop Solo e Duo. Só
-				teremos inscrições abertas no dia caso ainda haja vagas remanescentes da
-				inscrição online ou para lista de espera. Não aceitaremos inscrição por
-				procuração ou representação. Caso queira saber se ainda temos vagas
-				(para apresentação ou lista de espera), entre em contato pelo Whatsapp
-				(19) 98933-4566. Não esqueça de levar no dia do evento: - Documento com
-				foto (mesmo que utilizado no momento da inscrição). - Pendrive ou
-				celular contendo seu áudio (somente em MP3) a ser utilizado na
-				apresentação. DA APRESENTAÇÃO A sequência das apresentações no dia do
-				evento será correspondente ao recebimento das inscrições. Somente serão
-				permitidas apresentação de músicas do gênero pop coreano com no máximo
-				dois (02) minutos e trinta (30) segundos de duração (Introduções,
-				pausas, complementos, modificações (remix), mashup ou encerramentos não
-				correspondentes às coreografias originais poderão ser feitos, desde que
-				não excedam a duração máxima da apresentação). Caso o arquivo de áudio
-				exceda o tempo estabelecido, será necessário envio de edição da música
-				dentro do tempo estabelecido. Participante prejudicado por falhas na
-				aparelhagem de som, poderá reiniciar sua apresentação ou repeti-la ao
-				final das apresentações. Os participantes devem se apresentar ao lado do
-				Palco Anime Fest 20 minutos antes do horário marcado para início do
-				Concurso. A organização não disponibilizará tomadas ou qualquer
-				instalação elétrica auxiliar no palco e não se responsabiliza por
-				equipamentos ou itens levados pelos participantes. Na modalidade Solo é
-				permitida apenas uma (01) pessoa no palco. Na modalidade Duo é permitida
-				apenas duas (02) pessoas no palco. AVALIAÇÃO O corpo de júri será
-				composto por 03 (três) membros escolhidos pela organização, cujas notas
-				serão consideradas plenas e também inquestionáveis. Cada membro dará a
-				cada apresentação 03 (três) notas: - Execução de passos/sincronia;
-				Presença de palco/expressão artística e Figurino; As notas serão de 50
-				(cinquenta) a 100 (cem), podendo ser apenas fracionadas em 5 (cinco)
-				pontos. A nota 00 (zero) será dada apenas aos participantes
-				desclassificados; o cálculo da nota final é realizado através de uma
-				média aritmética entre todas as notas de cada um dos membros do júri.
-				Vence o participante com maior nota final e os demais são classificados
-				de forma decrescente. Os desempates serão feitos pelos seguintes
-				critérios e ordens: 01 - Média geral (notas dos três júris) no quesito
-				Execução de passos/sincronia; 02 - Média geral no quesito Presença de
-				palco/expressão artística; 03 - Média geral no quesito Figurino; 04 -
-				Indicação do júri. PREMIAÇÃO As 03 (três) melhores apresentações Solo
-				serão premiadas, sendo: - Primeiro, segundo e terceiro lugar: Medalha e
-				kit de produtos dos expositores do evento; As 03 (três) melhores
-				apresentações Duo serão premiadas, sendo: - Primeiro, segundo e terceiro
-				lugar: Medalha e kit produtos dos expositores do evento; Os vencedores
-				serão anunciados no palco Anime Fest e os prêmios serão entregues no
-				ato; caso estejam ausentes, o prêmio pode ser retirado em local a
-				combinar até no máximo 01 (um) mês após o evento em questão. A premiação
-				não será enviada pelo correio. É PROIBIDO E PROVOCA A DESCLASSIFICAÇÃO
-				DO CONCURSO: O uso de cenários e/ou equipamentos que possam pôr em risco
-				o participante, o local, o público ou outros participantes; participação
-				de jurados e equipe organizadora do evento; Uso de qualquer tipo de
-				ofensas, constrangimentos ou apelo sexual e que firam as disposições
-				criminais; Atirar qualquer objeto para fora do palco ou pularem do
-				palco. Porte e uso dos seguintes itens: fogo, explosivos, fogos de
-				artifício, arma de fogo (mesmo descarregadas), armas brancas, líquidos
-				ou materiais viscosos, e qualquer objeto que possa representar perigo às
-				pessoas presentes; Uso de qualquer banner ou faixa divulgando algum site
-				ou empresa, sem autorização da organização do evento; Reiniciar a
-				apresentação em caso de erro de coreografia. DISPOSIÇÕES GERAIS Os
-				participantes cedem o direito de vídeo-imagem por prazo indeterminado,
-				para fins promocionais do Concurso Kpop Solo e Duo, para Avalon Eventos
-				e seus parceiros; Ao participar do concurso os participantes autorizam o
-				envio de e-mails marketing com novidades sobre os eventos; O evento não
-				é obrigado a oferece nenhum local reservado à caracterização dos
-				participantes; Os inscritos devem pagar normalmente a entrada do evento
-				para participar do concurso; Os casos omissos no regulamento serão
-				analisados pela coordenação e equipe organizadora do Concurso Kpop Solo
-				e Duo e a sua decisão será inquestionável. As regras podem sofrer
-				alterações sem aviso prévio. Última alteração: 28/03/2023. ANEXO 01:
-				REFERÊNCIA DE FIGURINO A referência é uma imagem do figurino original no
-				qual foi baseado a confecção do figurino do participante. Ela serve para
-				que o corpo de júri possa avaliar o quanto seu figurino está fiel ao
-				figurino original. A entrega não é obrigatória, mas ajuda o corpo de
-				júri a fazer uma boa avaliação. A referência pode ser entregue no
-				momento da inscrição online ou após o contato da organização via
-				WhatsApp, informando o link (URL) de onde está a imagem na internet.
-				Caso sua referência esteja em alguma nuvem (Google drive, Onedrive,
-				etc), não esqueça de liberar o acesso a referência para qualquer pessoa
-				que tiver o link e criar link específico para o arquivo da referência,
-				não aceitaremos link para pasta. Caso faça a inscrição no dia do evento,
-				informe o link (URL) pelo nosso Whatsapp no momento da inscrição ou
-				entregue em um pendrive. A imagem deve seguir as seguintes
-				especificações para que possa ser avaliada em detalhes pelo corpo de
-				júri: - Formato: .JPG ou .PNG. - Dimensões requeridas: 570 pixels x 1000
-				pixels. - Tamanho máximo: 1 mega.
+
+				<h3>REGULAMENTO DO CONCURSO KPOP SOLO E DUO 2023</h3>
+				<br />
+				<h4>1. INTRODUÇÃO</h4>
+				<p>
+					O Concurso Kpop Solo e Duos é um concurso de dança pop coreana onde os participantes
+					fazem apresentações individuais ou em dupla. Qualquer pessoa com mais de
+					10 anos pode participar. São bem vindos profissionais,
+					pré-profissionais, amadores e iniciantes. No momento da inscrição os
+					participantes escolhem entre a modalidade Solo e Duo.
+					Cada participante poderá participar de apenas uma apresentação por concurso, não sendo
+					permitido que o mesmo participante faça apresentações como solo e duo.
+				</p>
+				<br />
+				<h4>2. INSCRIÇÃO</h4>
+				<p>
+					As inscrições podem ser realizadas em dois momento:
+					<br />
+					<span className="underline">- Inscrição online:</span> Através de formulário pela internet. O início das
+					inscrições e liberação do link do formulário de inscrição serão
+					anunciados com antecedência em nossa página do Facebook(
+					<a href="www.facebook.com/CircuitoAnimeFest">
+						www.facebook.com/CircuitoAnimeFest
+					</a>	
+					) e Instagram(
+					<a href="https://www.instagram.com/animefest/">
+						https://www.instagram.com/animefest/
+					</a>	
+					). No cabeçalho do formulário será
+					informado a quantidade máxima de vagas no concurso e data e horário
+					limite das inscrições online. Após a inscrição realizada, o participante
+					será contatado pelo Whatsapp informado na inscrição em até 48 horas. A
+					presença deve ser confirmada, somente pela pessoa que fez a inscrição,
+					no dia do evento até 30 (trinta) minutos antes do início do Concursos
+					Kpop Solo e Duo na fila exclusiva de concursos ou tenda de inscrições,
+					próximo ao palco Anime Fest. <strong>A não confirmação no dia, provoca sua
+					desclassificação</strong>.
+				</p>
+				<br />  
+				<p>
+					<strong>Dados necessários para efetuar a inscrição:</strong>
+					<br />
+					E-mail, Nome, Nome social ou artístico, Documento (RG/CPF/CNH), Data de
+					Nascimento, Telefone (Whatsapp), Cidade e Estado, Modalidade (Solo ou
+					Duo), Nome do Integrante 02. Nome da Música, Origem da Música, Link para
+					a Música, Link para Referência de Figurino (não obrigatório).
+				</p>
+				<br />
+				<p>
+				Vide anexo 01 do regulamento (ao final do documento) para detalhes sobre Referência de Figurino.
+				</p>
+				<br />
+				<p>
+					<span className="underline">- Inscrições no dia do evento:</span> Abrem às 11h00 e vão até 30
+					(trinta) minutos antes do início do Concursos Kpop Solo e Duo. Só
+					teremos inscrições abertas no dia caso ainda haja vagas remanescentes da
+					inscrição online ou para lista de espera. Não aceitaremos inscrição por
+					procuração ou representação. Caso queira saber se ainda temos vagas
+					(para apresentação ou lista de espera), entre em contato pelo Whatsapp
+					(19) 98933-4566.
+				</p>
+				<br />
+				<p>
+					<span className="underline">Não esqueça de levar no dia do evento:</span>
+					<br />
+					 - Documento com foto (mesmo que utilizado no momento da inscrição). 
+					 - Pendrive ou celular contendo seu áudio (somente em MP3) a ser utilizado na apresentação.
+				</p>
+				<br />
+				<h4>3. DA APRESENTAÇÃO</h4>
+				<p>
+					A sequência das apresentações no dia do
+					evento será correspondente ao recebimento das inscrições. Somente serão
+					permitidas apresentação de músicas do gênero pop coreano com no máximo
+					<strong>dois (02) minutos e trinta (30) segundos de duração</strong> (Introduções,
+					pausas, complementos, modificações (remix), mashup ou encerramentos não
+					correspondentes às coreografias originais poderão ser feitos, desde que
+					não excedam a duração máxima da apresentação). Caso o arquivo de áudio
+					exceda o tempo estabelecido, será necessário envio de edição da música
+					dentro do tempo estabelecido. Participante prejudicado por falhas na
+					aparelhagem de som, poderá reiniciar sua apresentação ou repeti-la ao
+					final das apresentações. Os participantes devem se apresentar ao lado do
+					Palco Anime Fest 20 minutos antes do horário marcado para início do
+					Concurso. A organização não disponibilizará tomadas ou qualquer
+					instalação elétrica auxiliar no palco e não se responsabiliza por
+					equipamentos ou itens levados pelos participantes. Na modalidade Solo é
+					permitida apenas uma (01) pessoa no palco. Na modalidade Duo é permitida
+					apenas duas (02) pessoas no palco.
+				</p>
+				<br />
+				<h4>4. AVALIAÇÃO</h4>
+				<p>
+					O corpo de júri será composto por 03 (três) membros escolhidos pela organização, cujas notas
+					serão consideradas plenas e também inquestionáveis. Cada membro dará a
+					cada apresentação 03 (três) notas: 
+					<br />
+					<strong>- Execução de passos/sincronia; Presença de palco/expressão artística e Figurino;</strong>
+					<br />
+					As notas serão de 50 (cinquenta) a 100 (cem), podendo ser apenas fracionadas em 5 (cinco)
+					pontos. A nota 00 (zero) será dada apenas aos participantes
+					desclassificados; o cálculo da nota final é realizado através de uma
+					média aritmética entre todas as notas de cada um dos membros do júri.
+					Vence o participante com maior nota final e os demais são classificados
+					de forma decrescente. Os desempates serão feitos pelos seguintes
+					critérios e ordens: 01 - Média geral (notas dos três júris) no quesito
+					Execução de passos/sincronia; 02 - Média geral no quesito Presença de
+					palco/expressão artística; 03 - Média geral no quesito Figurino; 04 -
+					Indicação do júri.
+				</p>
+				<br />
+				<h4>5. PREMIAÇÃO</h4>
+				<p>
+					As 03 (três) melhores apresentações Solo serão premiadas, sendo: 
+					<br />
+					- Primeiro, segundo e terceiro lugar: Medalha e kit de produtos dos expositores do evento; 
+					<br />
+					As 03 (três) melhores apresentações Duo serão premiadas, sendo:
+					<br />
+					- Primeiro, segundo e terceiro lugar: Medalha e kit produtos dos expositores do evento; 
+					<br /><br />
+					Os vencedores serão anunciados no palco Anime Fest e os prêmios serão entregues no
+					ato; caso estejam ausentes, o prêmio pode ser retirado em local a
+					combinar até no máximo 01 (um) mês após o evento em questão. A premiação
+					não será enviada pelo correio.
+				</p>
+				<br />
+				<h4>6. É PROIBIDO E PROVOCA A DESCLASSIFICAÇÃO DO CONCURSO:</h4>
+				<p>
+					O uso de cenários e/ou equipamentos que possam pôr em risco
+					o participante, o local, o público ou outros participantes; participação
+					de jurados e equipe organizadora do evento; Uso de qualquer tipo de
+					ofensas, constrangimentos ou apelo sexual e que firam as disposições
+					criminais; Atirar qualquer objeto para fora do palco ou pularem do
+					palco. Porte e uso dos seguintes itens: fogo, explosivos, fogos de
+					artifício, arma de fogo (mesmo descarregadas), armas brancas, líquidos
+					ou materiais viscosos, e qualquer objeto que possa representar perigo às
+					pessoas presentes; Uso de qualquer banner ou faixa divulgando algum site
+					ou empresa, sem autorização da organização do evento; Reiniciar a
+					apresentação em caso de erro de coreografia.
+				</p>
+				<br />
+				<h4>DISPOSIÇÕES GERAIS</h4>
+				<p>
+					Os participantes cedem o direito de vídeo-imagem por prazo indeterminado,
+					para fins promocionais do Concurso Kpop Solo e Duo, para Avalon Eventos
+					e seus parceiros; Ao participar do concurso os participantes autorizam o
+					envio de e-mails marketing com novidades sobre os eventos; O evento não
+					é obrigado a oferece nenhum local reservado à caracterização dos
+					participantes; Os inscritos devem pagar normalmente a entrada do evento
+					para participar do concurso; Os casos omissos no regulamento serão
+					analisados pela coordenação e equipe organizadora do Concurso Kpop Solo
+					e Duo e a sua decisão será inquestionável. As regras podem sofrer
+					alterações sem aviso prévio. Última alteração: 28/03/2023.
+				</p>
+				<br />
+				<h4>ANEXO 01: REFERÊNCIA DE FIGURINO</h4>
+				<p>
+					A referência é uma imagem do figurino original no
+					qual foi baseado a confecção do figurino do participante. Ela serve para
+					que o corpo de júri possa avaliar o quanto seu figurino está fiel ao
+					figurino original. A entrega não é obrigatória, mas ajuda o corpo de
+					júri a fazer uma boa avaliação.
+				</p>
+				<br />
+				<p>
+					A referência pode ser entregue no
+					momento da inscrição online ou após o contato da organização via
+					WhatsApp, informando o link (URL) de onde está a imagem na internet.
+					Caso sua referência esteja em alguma nuvem (Google drive, Onedrive,
+					etc), não esqueça de liberar o acesso a referência para qualquer pessoa
+					que tiver o link e criar link específico para o arquivo da referência,
+					não aceitaremos link para pasta. Caso faça a inscrição no dia do evento,
+					informe o link (URL) pelo nosso Whatsapp no momento da inscrição ou
+					entregue em um pendrive.
+				</p>
+				<br />
+				<p>
+					A imagem deve seguir as seguintes especificações para que possa ser avaliada em detalhes pelo corpo de júri:
+					<br />
+					- Formato: .JPG ou .PNG.
+					<br />
+					- Dimensões requeridas: 570 pixels x 1000 pixels. - Tamanho máximo: 1 mega.
+				</p>
 			</Box>
 		);
 	}
