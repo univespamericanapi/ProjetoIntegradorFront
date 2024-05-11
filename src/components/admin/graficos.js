89344a0eca3f6a0b74b34ff56ccca9ccadebe027
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-05-11 00:38:13 -0300

    feat: implantado gráfico Análise de Tendência: Temas

--- src/components/admin/graficos.js
+++ src/components/admin/graficos.js
@@ -10,15 +10,20 @@ export class Graficos extends Component {
         super(props);
         this.state = {
             listaEventos: [],
+            listaConcursos: [],
             dataConcurso: {},
             dataCidade: {},
             dataFaixaEtaria: {},
+            dataTemas: {},
             dadosCarregados: false,
+            dadosCarregadosConc: false,
             values: {
                 conc_event: 0,
                 conc_event_nome: '',
+                part_conc: 0,
+                part_conc_nome: '',
             },
-            selectBoxStyle: { width: '90%' },
+            selectBoxStyle: { width: '45%' },
         };
 
         this.state.initialValues = { ...this.state.values };
@@ -26,6 +31,9 @@ export class Graficos extends Component {
         this.fetchEventos = this.fetchEventos.bind(this);
         this.eventoSelecionado = this.eventoSelecionado.bind(this);
         this.fetchEventos();
+        this.fetchConcursos = this.fetchConcursos.bind(this);
+        this.concursoSelecionado = this.concursoSelecionado.bind(this);
+        this.fetchConcursos();
     }
 
     componentDidMount() {
@@ -70,7 +78,7 @@ export class Graficos extends Component {
                 this.state.dataConcurso,
                 "grafico-concurso",
                 "grafico-concurso-place",
-                "pie"
+                "polarArea"
             );
         }
 
@@ -79,6 +87,15 @@ export class Graficos extends Component {
                 this.state.dataFaixaEtaria,
                 "grafico-etario",
                 "grafico-etario-place",
+                "bar"
+            );
+        }
+
+        if (prevState.dataTemas !== this.state.dataTemas) {
+            this.criarGrafico(
+                this.state.dataTemas,
+                "grafico-temas",
+                "grafico-temas-place",
                 "bar"
             );
         }
@@ -108,17 +125,52 @@ export class Graficos extends Component {
         }
     }
 
+    async fetchConcursos(part_event) {
+        try {
+            const response = await api.get('/lista/concurso/' + part_event);
+            const concursos = response.data.map((concurso) => ({
+                part_conc: concurso.conc_id,
+                part_conc_nome: concurso.conc_nome,
+            }));
+            this.setState({ listaConcursos: concursos });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     eventoSelecionado = async (event, value) => {
         try {
             this.handleChangeAutocomplete(value);
             const dataCidade = await api.get(`/admin/grafico/cidade/${value.conc_event}`);
             const dataConcurso = await api.get(`/admin/grafico/concurso/${value.conc_event}`);
             const dataFaixaEtaria = await api.get(`/admin/grafico/faixas-etarias/${value.conc_event}`);
+            this.setState((prevState, props) => {
+                return {
+                    dadosCarregados: true,
+                    dadosCarregadosConc: false,
+                    dataCidade: dataCidade.data,
+                    dataConcurso: dataConcurso.data,
+                    dataFaixaEtaria: dataFaixaEtaria.data,
+                    values: {
+                        ...prevState.values,
+                        part_conc: 0,
+                        part_conc_nome: '',
+                    },
+                }
+            });
+            await this.fetchConcursos(value.conc_event);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    concursoSelecionado = async (conc, value) => {
+        try {
+            this.handleChangeAutocomplete(value);
+            const dataTemas = await api.get(`/admin/grafico/temas/${value.part_conc}`);;
             this.setState({
-                dadosCarregados: true,
-                dataCidade: dataCidade.data,
-                dataConcurso: dataConcurso.data,
-                dataFaixaEtaria: dataFaixaEtaria.data,
+                dadosCarregadosConc: true,
+                dataTemas: dataTemas.data,
             });
         } catch (error) {
             console.error(error);
@@ -133,29 +185,72 @@ export class Graficos extends Component {
             const newCanvas = document.createElement('canvas');
             newCanvas.id = id;
             canvasPlace.appendChild(newCanvas);
+            const eixo = id === 'grafico-etario' ? 'y' : 'x';
             const grafico = new Chart(newCanvas, {
                 type: type,
                 data: data,
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                    },
+                    title: {
+                        display: false,
+                    },
+                    maintainAspectRatio: false,
+                    indexAxis: eixo,
+                },
             });
-            grafico.legend.options.display = false;
             return grafico;
         }
         return null;
     };
 
     render() {
-        const { conc_event, conc_event_nome } = this.state.values;
-        const { listaEventos, dadosCarregados, selectBoxStyle } = this.state;
+        const { conc_event, conc_event_nome, part_conc, part_conc_nome } = this.state.values;
+        const { listaEventos, listaConcursos, dadosCarregados, dadosCarregadosConc, selectBoxStyle } = this.state;
+        const graphBox = {
+            width: '22%',
+            maxHeight: 240,
+            height: 240,
+            marginTop: '1.875rem',
+            display: 'flex',
+            position: 'relative',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(113, 113, 113, 0.1)',
+            padding: '36px',
+            borderRadius: '10px',
+            boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.2)',
+            transition: 'transform 0.3s ease',
+            transform: 'scale(1)',
+            '&:hover': {
+                transform: 'scale(1.2)',
+                zIndex: '100',
+            },
+        };
+        const graphTitle = {
+            fontSize: '12px',
+            marginBottom: '20px',
+            fontWeight: '700',
+            color: '#ff5722',
+            position: 'absolute',
+            top: 0,
+            marginTop: '10px',
+        };
 
         return (
             <Box sx={{ width: '100%' }}>
                 <Box
                     sx={{
+                        transform: '',
                         width: '100%',
                         marginTop: '1.875rem',
                         display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
+                        justifyContent: 'space-around',
                         alignItems: 'center',
                     }}
                 >
@@ -187,11 +282,38 @@ export class Graficos extends Component {
                         }}
                         disableClearable
                     />
+                    <Autocomplete
+                        id="cad-select-conc"
+                        getOptionLabel={(listaConcursos) =>
+                            `${listaConcursos.part_conc_nome}`
+                        }
+                        options={listaConcursos}
+                        sx={selectBoxStyle}
+                        isOptionEqualToValue={(option, value) =>
+                            option.part_conc_nome === value.part_conc_nome
+                        }
+                        noOptionsText={'Nenhum concurso está disponível.'}
+                        renderOption={(props, listaConcursos) => (
+                            <Box component="li" {...props} key={listaConcursos.part_conc}>
+                                {listaConcursos.part_conc_nome}
+                            </Box>
+                        )}
+                        renderInput={(params) => (
+                            <TextField {...params} label="Concurso" variant="outlined" />
+                        )}
+                        onChange={(conc, values, select, option) => {
+                            this.concursoSelecionado(conc, values, select, option);
+                        }}
+                        value={{
+                            part_conc: part_conc,
+                            part_conc_nome: part_conc_nome,
+                        }}
+                        disableClearable
+                    />
                 </Box>
                 {dadosCarregados &&
                     <Box sx={{
                         width: '100%',
-                        marginTop: '1.875rem',
                         display: 'flex',
                         justifyContent: 'space-around',
                         alignItems: 'center',
@@ -201,77 +323,69 @@ export class Graficos extends Component {
                         <Box
                             sx={{
                                 width: '100%',
-                                marginTop: '1.875rem',
+                                marginTop: '1rem',
                                 display: 'flex',
                                 justifyContent: 'space-around',
                                 alignItems: 'center',
                             }}
                         >
                             <Box
-                                sx={{
-                                    width: '18%',
-                                    marginTop: '1.875rem',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                }}
+                                sx={graphBox}
                                 id="grafico-cidade-place"
                             >
-                                <Typography sx={{
-                                    fontSize: '12px',
-                                    marginBottom: '20px',
-                                    color: '#572d7f',
-                                }}>
-                                    Competidores / Cidade
+                                <Typography sx={graphTitle}>
+                                    Análise Demográfica
                                 </Typography>
                                 <canvas id="grafico-cidade" />
                             </Box>
                             <Box
-                                sx={{
-                                    width: '18%',
-                                    marginTop: '1.875rem',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                }}
+                                sx={graphBox}
                                 id="grafico-concurso-place"
                             >
-                                <Typography sx={{
-                                    fontSize: '12px',
-                                    marginBottom: '20px',
-                                    color: '#572d7f',
-                                }}>
-                                    Competidores / Concurso
+                                <Typography sx={graphTitle}>
+                                    Análise de Tendência: Concursos
                                 </Typography>
                                 <canvas id="grafico-concurso" />
                             </Box>
                             <Box
-                                sx={{
-                                    width: '28%',
-                                    marginTop: '1.875rem',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                }}
+                                sx={graphBox}
                                 id="grafico-etario-place"
                             >
-                                <Typography sx={{
-                                    fontSize: '12px',
-                                    marginBottom: '20px',
-                                    color: '#572d7f',
-                                }}>
-                                    Faixa Etária
+                                <Typography sx={graphTitle}>
+                                    Análise Etária
                                 </Typography>
                                 <canvas id="grafico-etario" />
                             </Box>
+                            {dadosCarregadosConc ?
+                                <Box
+                                    sx={graphBox}
+                                    id="grafico-temas-place"
+                                >
+                                    <Typography sx={graphTitle}>
+                                        Análise de Tendência: Temas
+                                    </Typography>
+                                    <canvas id="grafico-temas" />
+                                </Box> :
+                                <Box
+                                    sx={graphBox}
+                                    id="grafico-temas-place"
+                                >
+                                    <Typography sx={graphTitle}>
+                                        Análise de Tendência: Temas
+                                    </Typography>
+                                    <Typography sx={{
+                                        ...graphTitle,
+                                        position: 'initial',
+                                        textAlign: 'center'
+                                    }}>
+                                        Selecione o Concurso para carregar o gráfico.
+                                    </Typography>
+                                </Box>}
                         </Box>
                         <Box
                             sx={{
                                 width: '100%',
-                                marginTop: '1.875rem',
+                                marginTop: '1rem',
                                 display: 'flex',
                                 justifyContent: 'space-around',
                                 alignItems: 'center',
